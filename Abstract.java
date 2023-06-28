9a508e7c8eb70b7faf7e51a64fc4c92e9c0bfce6
sgkrish19 <137775828+sgkrish19@users.noreply.github.com>
2023-06-28 13:03:33 +0530

    Add files via upload

---
+++ Abstract.java
@@ -0,0 +1,20 @@
+package Abstraction;
+
+abstract class Shape{  
+        abstract void draw();  
+        }  
+        class Rectangle extends Shape{  
+        void draw(){System.out.println("drawing rectangle");}  
+        }  
+        class Circle1 extends Shape{  
+        void draw(){System.out.println("drawing circle");}  
+        }  
+        public class Abstract {
+        public static void main(String args[]){  
+        Shape s=new Circle1(); 
+        s.draw();  
+        Shape s1=new Rectangle(); 
+        s1.draw(); 
+        }  
+        } 
+
